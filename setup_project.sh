7d5fa690d08346cbfdc99abf5c05b2ab144fdf1e
Kingsley Kipkoech <k.kipkoech@alustudent.com>
2026-06-08 02:50:59 +0200

    second part of  the  scriptn

--- setup_project.sh
+++ setup_project.sh
@@ -14,9 +14,16 @@ fi
 
 echo "Doing the  creation  of  the  workspace"
 read -p "Enter a  unique  folder  name: " folder
-name="attendance_tracker_{$folder}"
-mkdir -p "$folder"
-mkdir -p "$folder/Helpers"
-mkdir -p "$folder/reports"
+name="attendance_tracker_$folder"
+if [ -d $name ];then
+	echo "exists"
+else
+mkdir -p "$name"
+mkdir -p "$name/Helpers"
+mkdir -p "$name/reports"
 echo "Parent directory and files  created for the  workspaces"
-
+touch "$name/attendance_checker.py"
+touch "$name/Helpers/assets.csv"
+touch "$name/Helpers/config.json"
+touch "$name/reports/reports.log"
+fi
