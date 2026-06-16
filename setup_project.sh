996de6a7033e3ab31434156b15d508ae9fd901cf
Kingsley Kipkoech <k.kipkoech@alustudent.com>
2026-06-16 20:10:26 +0200

    finalisation

--- setup_project.sh
+++ setup_project.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-interrupt() {
+CTRL_C() {
 	echo " interrupt detected "
 	echo " cleanning  up  ... "
 	if [ -d "$name" ];then
-	tar cf "${name}_archive.tar" "$name"
+	tar cf "${name}_archive" "$name"
 	rm -rf "$name"
 	echo "Incompleted structure  removed"
 	fi
 	exit 1
 }
-trap interrupt SIGINT
+trap CTRL_C SIGINT
 #THIS  IS  THE  INSTRUCTION  THE  PROGRAM  WILL FOLLOW, THATS  WHY  ITS  AT THE  TOP
 
 
@@ -37,7 +37,7 @@ else
 sleep 2
 echo "Parent directory and files  created for the  workspaces"
 
-# creating attendance_checker.py file
+# making  of attendance_checker.py file
 cat > "$name/attendance_checker.py" << 'EOF'
 import csv
 import json
@@ -86,7 +86,7 @@ if __name__ == "__main__":
     run_attendance_check()
 EOF
 
-#CONFIG.json file
+#making CONFIG.json file
 cat > "$name/Helpers/config.json" << 'EOF'
 {
     "thresholds": {
@@ -98,7 +98,7 @@ cat > "$name/Helpers/config.json" << 'EOF'
 }
 EOF
 
-#assets.csv file
+#making assets.csv file
 cat > "$name/Helpers/assets.csv" << 'EOF'
 Names,Email,Attendance Count
 Alice Johnson,alice@example.com,14
@@ -108,7 +108,7 @@ Diana Ross,diana@example.com,12
 Edward King,edward@example.com,10
 EOF
 
-#reports.log file
+#making reports.log file
 cat > "$name/reports/reports.log" << 'EOF'
 --- Attendance Report Run: 2026-02-06 18:10:01.468726 ---
 [2026-02-06 18:10:01.469363] ALERT SENT TO bob@example.com: URGENT: Bob Smith, your
@@ -120,8 +120,8 @@ EOF
 sleep 2
 fi
 
-read -p "Do you want to update the attendance thresholds? (y/n): " choice
-if [ "$choice" = "y" ]; then
+read -p "Do you want to update the attendance thresholds? (y/n): " decision
+if [ "$decision" = "y" ]; then
         read -p "Enter Warning threshold in %:  " warning
         read -p "Enter Failure threshold %: " failure
         sed -i "s/75/$warning/" "$name/Helpers/config.json"
